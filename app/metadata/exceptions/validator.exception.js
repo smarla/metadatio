3b67713c843bc4d0cb02f2c2f31dd81bf72e0ef4
Pelayo Sánchez <sanchezmargareto@gmail.com>
2016-05-10 20:28:07 +0200

    including documentation on validator-exception

--- app/metadata/exceptions/validator.exception.js
+++ app/metadata/exceptions/validator.exception.js
@@ -2,7 +2,22 @@
  * Created by sm on 01/05/16.
  */
 
+/**
+ * 
+ *
+ * @module Core
+ * @submodule exceptions
+ * @class ValidatorException
+ * @constructor
+ */
 export default class ValidatorException extends Error {
+
+    /**
+     * Constructs a new exception
+     *
+     * @method constructor
+     * @param e
+     */
     constructor(e) {
         super(e);
         this.className = 'ValidatorException';
