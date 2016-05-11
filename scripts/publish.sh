b06e3c795ec1905ff176e248299ebd6c07e24902
Pelayo Sánchez <sanchezmargareto@gmail.com>
2016-05-11 22:25:11 +0200

    trying auto-publishing - v1

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,6 +8,48 @@ then
   exit 0
 fi
 
-comment=$(git log -1 --pretty=%B)
+#comment=$(git log -1 --pretty=%B)
+last_tag=$(git describe --abbrev=0)
 
-echo "${comment}";
+comment="[patch] Testing patch change"
+current_version="${last_tag//./ }"
+version_array=($current_version)
+change_type="None"
+
+major=${version_array[0]}
+minor=${version_array[1]}
+patch=${version_array[2]}
+
+path_regex='^\[patch\]';
+minor_regex='^\[minor\]';
+major_regex='^\[major\]';
+version_change=0
+if [[ $comment =~ $path_regex ]]
+then
+  change_type="patch"
+  version_change=1
+  patch=$((patch+1))
+
+elif [[ $comment =~ $minor_regex ]]
+then
+  change_type="minor"
+  version_change=1
+  minor=$((minor+1));
+
+elif [[ $comment =~ $major_regex ]]
+then
+  change_type="major"
+  version_change=1
+  major=$((major+1))
+fi
+
+new_version="${major}.${minor}.${patch}"
+
+if [[ ${version_change} == 1 ]]
+then
+  # Update package.json
+  echo "Updating package.json"
+  sed -i 's/${current_version}/${new_version}/g' package.json
+  git commit -am "${change_type} update to ${new_version}"
+  git push origin master
+fi
