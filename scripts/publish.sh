defa63d3a7930b261a5192efcc971559d1a8935d
Pelayo Sánchez <sanchezmargareto@gmail.com>
2016-05-11 19:03:39 +0200

    starting version publishing

---
+++ scripts/publish.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+comment=$(git log -1 --pretty=%B)
