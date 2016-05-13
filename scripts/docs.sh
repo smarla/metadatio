5cc6ae3f556d6e45d3e3ef004ed12e56ee8db73d
Pelayo Sánchez Margareto <sanchezmargareto@gmail.com>
2016-05-13 16:14:02 +0200

    fix some things

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -2,28 +2,26 @@
 
 set -o errexit -o nounset
 
-if [ "$TRAVIS_BRANCH" != "master" ]
+if [ "$TRAVIS_BRANCH" == "master" ]
 then
-  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
-  exit 0
+
+    rev=$(git rev-parse --short HEAD)
+
+    cd docs
+
+    git init
+    git config user.name "Pelayo Sánchez Margareto"
+    git config user.email "sanchezmargareto@gmail.com"
+
+    git remote add upstream "https://$GH_TOKEN@github.com/smarla/metadatio.git"
+    git fetch upstream
+    git reset upstream/gh-pages
+
+    echo "metadat.io" > CNAME
+
+    touch .
+
+    git add -A .
+    git commit -m "rebuild pages at ${rev}"
+    git push -q upstream HEAD:gh-pages
 fi
-
-rev=$(git rev-parse --short HEAD)
-
-cd docs
-
-git init
-git config user.name "Pelayo Sánchez Margareto"
-git config user.email "sanchezmargareto@gmail.com"
-
-git remote add upstream "https://$GH_TOKEN@github.com/smarla/metadatio.git"
-git fetch upstream
-git reset upstream/gh-pages
-
-echo "metadat.io" > CNAME
-
-touch .
-
-git add -A .
-git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,71 +2,69 @@
 
 set -o errexit -o nounset
 
-if [ "$TRAVIS_BRANCH" != "master" ]
+if [ "$TRAVIS_BRANCH" == "master" ]
 then
-  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
-  exit 0
-fi
 
-comment=$(git log -1 --pretty=%B)
-#last_tag=$(git describe --abbrev=0)
+    comment=$(git log -1 --pretty=%B)
+    #last_tag=$(git describe --abbrev=0)
 
-last_tag=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+    last_tag=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 
-current_version="${last_tag//./ }"
-version_array=($current_version)
-change_type="None"
+    current_version="${last_tag//./ }"
+    version_array=($current_version)
+    change_type="None"
 
-major=${version_array[0]}
-minor=${version_array[1]}
-patch=${version_array[2]}
+    major=${version_array[0]}
+    minor=${version_array[1]}
+    patch=${version_array[2]}
 
-path_regex='^\[patch\]';
-minor_regex='^\[minor\]';
-major_regex='^\[major\]';
-version_change=0
-if [[ $comment =~ $path_regex ]]
-then
-  change_type="patch"
-  version_change=1
-  patch=$((patch+1))
+    path_regex='^\[patch\]';
+    minor_regex='^\[minor\]';
+    major_regex='^\[major\]';
+    version_change=0
+    if [[ $comment =~ $path_regex ]]
+    then
+      change_type="patch"
+      version_change=1
+      patch=$((patch+1))
 
-elif [[ $comment =~ $minor_regex ]]
-then
-  change_type="minor"
-  version_change=1
-  minor=$((minor+1));
-  patch=0
+    elif [[ $comment =~ $minor_regex ]]
+    then
+      change_type="minor"
+      version_change=1
+      minor=$((minor+1));
+      patch=0
 
-elif [[ $comment =~ $major_regex ]]
-then
-  change_type="major"
-  version_change=1
-  major=$((major+1))
-  minor=0
-  patch=0
-fi
+    elif [[ $comment =~ $major_regex ]]
+    then
+      change_type="major"
+      version_change=1
+      major=$((major+1))
+      minor=0
+      patch=0
+    fi
 
-new_version="${major}.${minor}.${patch}"
-export version_change;
+    new_version="${major}.${minor}.${patch}"
+    export version_change;
 
-if [[ ${version_change} == 1 ]]
-then
-  package="package.json"
-  yui="yuidoc.json"
-  sed -i "s/\"version\": \"$last_tag\"/\"version\": \"$new_version\"/g" "$package"
-  sed -i "s/\"version\": \"${last_tag}\"/\"version\": \"${new_version}\"/g" "$yui"
-  git config user.name "Pelayo Sánchez Margareto"
-  git config user.email "sanchezmargareto@gmail.com"
+    if [[ ${version_change} == 1 ]]
+    then
+      package="package.json"
+      yui="yuidoc.json"
+      sed -i "s/\"version\": \"$last_tag\"/\"version\": \"$new_version\"/g" "$package"
+      sed -i "s/\"version\": \"${last_tag}\"/\"version\": \"${new_version}\"/g" "$yui"
+      git config user.name "Pelayo Sánchez Margareto"
+      git config user.email "sanchezmargareto@gmail.com"
 
-  git remote add upstream "https://$GH_TOKEN@github.com/smarla/metadatio.git"
-  git fetch upstream
+      git remote add upstream "https://$GH_TOKEN@github.com/smarla/metadatio.git"
+      git fetch upstream
 
-  git tag $new_version
-  git push upstream --tags
+      git tag $new_version
+      git push upstream --tags
 
-  git commit -am "[${change_type} RELEASE] updated to ${new_version} [ci skip]"
+      git commit -am "[${change_type} RELEASE] updated to ${new_version} [ci skip]"
 
 
-  git push --tags upstream HEAD:master
+      git push --tags upstream HEAD:master
+    fi
 fi
