c4db94168c6e75d27bc7024140d6afcf87bf1a99
Pelayo Sánchez <sanchezmargareto@gmail.com>
2016-05-14 02:42:35 +0200

    [patch] separating version for tag for using in other steps

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -2,48 +2,6 @@
 
 set -o errexit -o nounset
 
-comment=$(git log -1 --pretty=%B)
-#last_tag=$(git describe --abbrev=0)
-
-last_tag=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
-
-current_version="${last_tag//./ }"
-version_array=($current_version)
-change_type="None"
-
-major=${version_array[0]}
-minor=${version_array[1]}
-patch=${version_array[2]}
-
-path_regex='^\[patch\]';
-minor_regex='^\[minor\]';
-major_regex='^\[major\]';
-version_change=0
-if [[ $comment =~ $path_regex ]]
-then
-  change_type="patch"
-  version_change=1
-  patch=$((patch+1))
-
-elif [[ $comment =~ $minor_regex ]]
-then
-  change_type="minor"
-  version_change=1
-  minor=$((minor+1));
-  patch=0
-
-elif [[ $comment =~ $major_regex ]]
-then
-  change_type="major"
-  version_change=1
-  major=$((major+1))
-  minor=0
-  patch=0
-fi
-
-new_version="${major}.${minor}.${patch}"
-export version_change;
-
 if [[ ${version_change} == 1 ]]
 then
   package="package.json"
