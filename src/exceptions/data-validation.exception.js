ad6c0dfa77277b566c08ed4bbd215e5ce868ad01
Pelayo Sánchez <sanchezmargareto@gmail.com>
2016-05-17 22:49:34 +0200

    including documentation on reducer exception

--- src/exceptions/data-validation.exception.js
+++ src/exceptions/data-validation.exception.js
@@ -2,6 +2,14 @@
  * Created by sm on 01/05/16.
  */
 
+/**
+ * This exception is thrown whenever there is an error while validating your data.
+ *
+ * @module Core
+ * @submodule exceptions
+ * @class DataValidationException
+ * @extends Error
+ */
 export default class DataValidationException extends Error {
     constructor(e) {
         super(e);
