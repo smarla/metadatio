5d8cfb8ebcb193ca87534e8409467a56f0abecd6
Pelayo Sanchez Margareto <pesama@smarla.com>
2017-02-01 23:33:59 +0100

    Removing React and Redux from the picture.

--- src/exceptions/index.js
+++ src/exceptions/index.js
@@ -3,24 +3,16 @@
  */
 
 import BaseException from './base.exception';
-import ActionException from './action.exception';
 import ItemException from './item.exception';
 import DataValidationException from './data-validation.exception';
 import MetadataIntegrityException from './metadata-integrity.exception';
 import MetadatioException from './metadatio.exception';
 import ValidatorException from './validator.exception';
 
-import ReducerException from './reducer.exception';
-import StoreException from './store.exception';
-
 module.exports = {
-    ActionException,
     ItemException,
     DataValidationException,
     MetadataIntegrityException,
     MetadatioException,
-    ValidatorException,
-
-    ReducerException,
-    StoreException
+    ValidatorException
 };
